00551905a3198e42d7af9f6e2efbd50bb8d0bbb4
FabCom <fabcom@communs.fr>
2022-06-09 13:38:06 +0200

    create page contact

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material"
+import DefaultLayout from "components/layouts/default"
+
+const Contact = () => {
+  return (
+    <DefaultLayout title="Suivez-nous">
+      <Typography variant="h2">Réseaux sociaux</Typography>
+      <Typography variant="h2">Newsletter</Typography>
+    </DefaultLayout>
+  )
+}
+
+export default Contact
